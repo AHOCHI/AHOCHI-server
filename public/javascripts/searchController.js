d50e2038cb604ed3759bc22f412b9a7319b8b665
bridewellc1 <bridewellc1@nku.edu>
2016-04-25 19:29:03 -0400

    Use angular material

--- public/javascripts/searchController.js
+++ public/javascripts/searchController.js
@@ -1,4 +1,4 @@
-var app = angular.module('Ahochi', ['ngResource', 'ngRoute']);
+var app = angular.module('Ahochi', ['ngResource', 'ngRoute', 'ngMaterial']);
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
