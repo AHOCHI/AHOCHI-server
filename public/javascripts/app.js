f8633cb0082d5168d3181baedca6b045a45474c3
bridewellc1 <bridewellc1@nku.edu>
2016-05-01 19:10:47 -0400

    Add easy way to change theme

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,10 @@
 var app = angular.module('Ahochi', ['ngResource', 'ngRoute', 'ngMaterial']);
+
+app.config(function($mdThemingProvider) {
+    //same as defaults currently
+    $mdThemingProvider.theme('default')
+        .primaryPalette('indigo')
+        .accentPalette('pink')
+        .warnPalette('red')
+        .backgroundPalette('grey');
+});
